e67ba759961a8ac882ad582c187a240d0c464443
behrlevi <sidolevente@aol.com>
2023-09-19 14:48:28 +0200

    Added database functionality

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+#The database model
+
+class Record(models.Model):
+	created_at=models.DateTimeField(auto_now_add=True)
+	first_name=models.CharField(max_length=50)
+	last_name=models.CharField(max_length=50)
+	email=models.CharField(max_length=100)
+	phone=models.CharField(max_length=15)
+	address=models.CharField(max_length=100)
+	city=models.CharField(max_length=50)
+	state=models.CharField(max_length=50)
+	zipcode=models.CharField(max_length=5)
+
+	def __str__(self):
+		return(f"{self.first_name} {self.last_name}")
