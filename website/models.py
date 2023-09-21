290b62b5ae3c75b59881349639a1b4eaa6cfda7e
behrlevi <sidolevente@aol.com>
2023-09-21 15:42:22 +0200

    Started the consolidation of models with the original intent.

--- website/models.py
+++ website/models.py
@@ -3,15 +3,12 @@ from django.db import models
 #The database model
 
 class Record(models.Model):
-	created_at=models.DateTimeField(auto_now_add=True)
 	first_name=models.CharField(max_length=50)
 	last_name=models.CharField(max_length=50)
-	email=models.CharField(max_length=100)
-	phone=models.CharField(max_length=15)
-	address=models.CharField(max_length=100)
-	city=models.CharField(max_length=50)
-	state=models.CharField(max_length=50)
-	zipcode=models.CharField(max_length=5)
+	company=models.CharField(max_length=20, default='ROSSI')
+	position=models.CharField(max_length=50)
+	created_at=models.DateTimeField(auto_now_add=True)
+	note=models.CharField(max_length=100, blank=True)
 
 	def __str__(self):
 		return(f"{self.first_name} {self.last_name}")
